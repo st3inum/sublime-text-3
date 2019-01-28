6ff3518ccb0f9ccf29c0d16b77643fb4bdd627f2
st3inum <st3inum@gmail.com>
2019-01-28 20:51:20 +0600

    Add files via upload

---
+++ sfml.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d /opt/sfml ]; then
+	wget https://www.sfml-dev.org/files/SFML-2.5.1-linux-gcc-64-bit.tar.gz
+	tar xvzf SFML-2.5.1-linux-gcc-64-bit.tar.gz
+	mv SFML-2.5.1 sfml
+	sudo mv sfml /opt/
+	rm SFML-2.5.1-linux-gcc-64-bit.tar.gz
+fi
+
+
+
